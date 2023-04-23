221ca860256f957127e0e6db0aca4ce1e543647e
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-23 14:37:12 +0300

    Add educationalManager model

---
+++ frameworks/database/MongoDB/models/educational_manager.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const EducationalManagerSchema = new Schema({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    idNumber: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    faculty: {
+        type: String
+    }
+});
+
+const EducationalManagerModel = mongoose.model('EducationalManager', EducationalManagerSchema);
+
+export default EducationalManagerModel;

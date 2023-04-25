3a46d1da1574c9f4f16682e644a6e342510ae792
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-26 02:22:40 +0300

    Add student router

---
+++ frameworks/webserver/routes/student.js
@@ -0,0 +1,24 @@
+import studentController from "../../../adapters/controllers/studentController.js";
+import studentDbRepository from "../../../application/repositories/studentRepository.js";
+import studentRepositoryMongoDB from "../../database/MongoDB/repositories/studentRepositoryMongoDB.js";
+
+const studentRouter = function (express) {
+
+    const router = express.Router()
+
+    const controller = studentController(studentDbRepository, studentRepositoryMongoDB)
+
+    router.post("/student", controller.addNewStudent)
+
+    router.get("/student/:id", controller.fetchStudentById())
+
+    router.get("/students", controller.fetchAllStudents())
+
+    router.delete("/student/:id", controller.deleteStudent)
+
+    router.put("/student", controller.updateStudent)
+
+    return router;
+
+}
+export default studentRouter;

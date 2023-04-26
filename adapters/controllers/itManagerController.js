3b5a176a5518b4e0fb6a7902919edbc50885a2ba
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-26 07:57:49 +0300

    Add itManagerController

---
+++ adapters/controllers/itManagerController.js
@@ -0,0 +1,98 @@
+import addItManager from "../../application/use_cases/it_manager/add.js";
+import findById from "../../application/use_cases/it_manager/findById.js";
+import findAll from "../../application/use_cases/it_manager/findAll.js";
+import deleteById from "../../application/use_cases/it_manager/deleteById.js";
+import update from "../../application/use_cases/it_manager/update.js";
+
+const professorController = function (
+    itManagerDbRepository,
+    itManagerDbRepositoryImpl
+) {
+
+    const internalServerError = {error: "Unknown error", errorCode: 500}
+
+    const itManagerRepository = itManagerDbRepository(itManagerDbRepositoryImpl())
+
+    const fetchAllItManagers = (req, res, next) => {
+
+        findAll({itManagerRepository}).then(managers => {
+            res.json({managers: managers})
+        }).catch(err => {
+            res.status(500).json(internalServerError)
+        })
+
+    }
+
+    const addNewItManager = (req, res, next) => {
+
+        const idNumber = Date.now()
+        const {firstName, lastName, email, password, phone} = req.body
+
+        addItManager(
+            {firstName, lastName, idNumber, password, email, phone, itManagerRepository}
+        ).then(manager => {
+            res.json({manager: manager})
+        }).catch(err => {
+            res.status(500).json(internalServerError)
+        })
+
+    }
+
+    const deleteItManager = (req, res, next) => {
+
+        const idNumber = req.params.id
+
+        deleteById({idNumber, itManagerRepository})
+            .then(manager => {
+                res.json({manager: manager})
+            }).catch(err => {
+            res.status(500).json(internalServerError)
+        })
+
+
+    }
+
+    const fetchItManagerById = (req, res, next) => {
+
+        const idNumber = req.params.id
+
+        findById({idNumber, itManagerRepository}).then(manager => {
+            res.json({manager: manager})
+        }).catch(err => {
+            res.status(500).json(internalServerError)
+        })
+
+    }
+
+    const updateItManager = (req, res, next) => {
+
+
+        const idNumber = req.params.id
+
+        const {firstName, lastName, email, password, rank, phone} = req.body
+
+        update(
+            {firstName, lastName, idNumber, password, email, phone, itManagerRepository}
+        ).then(manager => {
+            res.json({manager: manager})
+        }).catch(err => {
+            res.status(500).json(internalServerError)
+        })
+
+
+    }
+
+
+    return {
+        fetchAllItManagers,
+        addNewItManager,
+        deleteItManager,
+        fetchItManagerById,
+        updateItManager
+    }
+
+
+}
+
+
+export default professorController;

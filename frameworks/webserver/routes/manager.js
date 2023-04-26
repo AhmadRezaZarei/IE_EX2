44225dc1b43aefe92616721a51e06003d9824938
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-26 07:59:52 +0300

    Add manager routes

---
+++ frameworks/webserver/routes/manager.js
@@ -0,0 +1,24 @@
+import itManagerController from "../../../adapters/controllers/itManagerController.js";
+import itManagerRepository from "../../../application/repositories/itManagerRepository.js";
+import itManagerRepositoryMongoDB from "../../database/MongoDB/repositories/itManagerRepositoryMongoDB.js";
+
+const professorRouter = function (express) {
+
+    const router = express.Router()
+
+    const controller = itManagerController(itManagerRepository, itManagerRepositoryMongoDB)
+
+    router.post("manager/", controller.addNewProfessor)
+
+    router.get("manager/:id", controller.fetchProfessorById)
+
+    router.get("managers/", controller.fetchAllProfessors)
+
+    router.delete("manager/:id", controller.deleteProfessor)
+
+    router.put("manger/:id", controller.updateProfessor)
+
+    return router;
+
+}
+export default professorRouter;

3987766323beb839433ee2a689611eae291b5412
RayZarei <ahmadreza0zarei@gmail.com>
2023-05-03 18:15:43 +0300

    Add course routes

---
+++ frameworks/webserver/routes/course.js
@@ -0,0 +1,24 @@
+import courseController from "../../../adapters/controllers/courseController.js";
+import courseDbRepository from "../../../application/repositories/CourseRepository.js";
+import courseRepositoryMongoDB from "../../database/MongoDB/repositories/courseRepositoryMongoDB.js";
+
+const courseRouter = function (express) {
+
+    const router = express.Router()
+
+    const controller = courseController(courseDbRepository, courseRepositoryMongoDB)
+
+    router.post("/course/", controller.addNewCourse)
+
+    router.get("/course/:id", controller.fetchCourseById)
+
+    router.get("/courses", controller.fetchAllCourses)
+
+    router.delete("/course/:id", controller.deleteCourse)
+
+    router.put("/course/:id", controller.updateCourse)
+
+    return router;
+
+}
+export default courseRouter;

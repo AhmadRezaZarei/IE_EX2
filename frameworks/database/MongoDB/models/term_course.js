7f9af9428e3679dc1438c77db68f5b01954917e0
RayZarei <ahmadreza0zarei@gmail.com>
2023-05-09 15:11:20 +0300

    Add term_course

---
+++ frameworks/database/MongoDB/models/term_course.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const termCourseSchema = new Schema({
+    courseId: {
+        type: String,
+    },
+    courseDataAndTime: {
+        type: String,
+    },
+    examDataAndTime: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    professorId: {
+        type: String
+    },
+    capacity: {
+        type: String
+    },
+    term: {
+        type: String
+    },
+});
+
+const TermCourseModel = mongoose.model('TermCourse', termCourseSchema);
+
+export default TermCourseModel

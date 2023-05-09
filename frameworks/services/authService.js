0daf6d322938fe75731a85a241673ce66f9413a2
RayZarei <ahmadreza0zarei@gmail.com>
2023-05-09 15:41:04 +0300

    authService

---
+++ frameworks/services/authService.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+export default function authService() {
+    const encryptPassword = (password) => {
+        const salt = bcrypt.genSaltSync(10);
+        return bcrypt.hashSync(password, salt);
+    };
+
+    const compare = (password, hashedPassword) =>
+        bcrypt.compareSync(password, hashedPassword);
+
+    const verify = (token) => jwt.verify(token, "secret");
+
+    const generateToken = (payload) =>
+        jwt.sign(payload, "secret", {
+            expiresIn: 9999999999999
+        });
+
+    return {
+        encryptPassword,
+        compare,
+        verify,
+        generateToken
+    };
+}

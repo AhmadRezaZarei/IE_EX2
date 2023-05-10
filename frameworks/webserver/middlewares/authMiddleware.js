1a4cd3e8a66b6928083a7e55d7581956c0cf011c
RayZarei <ahmadreza0zarei@gmail.com>
2023-05-10 07:43:15 +0300

    Send standard error response in middleware

--- frameworks/webserver/middlewares/authMiddleware.js
+++ frameworks/webserver/middlewares/authMiddleware.js
@@ -5,17 +5,15 @@ export default function authMiddleware(req, res, next) {
     // Get token from header
     const token = req.header('Authorization');
     const authService = authServiceInterface(authServiceImpl());
-    if (!token) {
-        throw new Error('No access token found');
-    }
-    if (token.split(' ')[0] !== 'Bearer') {
-        throw new Error('Invalid access token format');
+    if (!token || token.split(' ')[0] !== 'Bearer') {
+        res.status(401).json({"error": "Invalid token", errorCode: 401});
+        return;
     }
     try {
         const decoded = authService.verify(token.split(' ')[1]);
         req.user = decoded.user;
         next();
     } catch (err) {
-        throw new Error('Token is not valid');
+        res.status(401).json({"error": "Invalid token", errorCode: 401})
     }
 }
